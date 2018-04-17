266063ccfccb4d5eb6375e93aae24a59a5808ce8
Bruno Blanchet <bruno.blanchet@inria.fr>
2018-04-17 12:33:27 +0000

    emacs .pcv mode

--- emacs/cryptoverif.el
+++ emacs/cryptoverif.el
@@ -66,3 +66,57 @@
   (modify-syntax-entry ?' "w" cryptoverifo-mode-syntax-table)
 )
 
+;;
+;; mode for .pcv files (compatibility CryptoVerif and ProVerif)
+;;
+
+(defvar pcv-kw '("new" "out" "channel" "if" "then" "else" "fun" "param" "forall" "equation" "proba" "type" "process" "let" "in" "query" "secret" "public_vars" "const" "set"  "event" "yield" "event_abort" "inj-event" "foreach" "do" "def" "expand" "proof" "implementation" "get" "insert" "table" "letfun" "suchthat" "not") "Cryptoverif and ProVerif common keywords")
+
+(defvar pcv-bad-kw '("among" "choice" "clauses" "diff" "elimtrue" "find" "orfind" "equivalence" "fail" "free" "noninterf" "nounif" "or" "otherwise" "phase" "pred" "putbegin" "reduc" "sync" "weaksecret" "builtin" "equiv" "defined" "collision" "time" "maxlength" "length" "max" "newChannel" "return") "CryptoVerif- or ProVerif-only keywords")
+
+(defvar pcv-builtin '("noninteractive" "bounded" "fixed" "large" "password" ;;type options (ignored in PV)
+		      "data" "projection" "uniform" "typeConverter" ;; function options
+		      "cv_onesession" "real_or_random" "cv_real_or_random" "pv_real_or_random" "pv_reachability" ;; query options
+		      "pred" "serial" "inverse" "random" ;; implementation options (ignored in PV)
+		      ) "Cryptoverif and ProVerif common builtins")
+
+(defvar pcv-bad-builtin '("commut" "assoc" "AC" "assocU" "ACU" "ACUN" "group" "commut_group" ;; equation builtin options in CV
+			  "manual" "computational" "unchanged" "exist" "all" "useful_change" ;; equiv options in CV
+			  "unique" ;; find option in CV
+			  "private" ;; in PV
+			  "reachability" ;; query option not supported in CV
+			  "memberOptim" "decompData" "decompDataSelect" "block" ;; predicate options
+			  "attacker" "mess" ;; predicates
+			  ) "CryptoVerif- or ProVerif-only builtins")
+
+;; build optimal regular expression from list of keywords
+;; 'words if for taking full words only, not subwords
+(defvar pcv-kw-regexp (regexp-opt pcv-kw 'words))
+(defvar pcv-builtin-regexp (regexp-opt pcv-builtin 'words))
+(defvar pcv-bad-kw-regexp (regexp-opt pcv-bad-kw 'words))
+(defvar pcv-bad-builtin-regexp (regexp-opt pcv-bad-builtin 'words))
+
+(defvar pcv-connectives-regexp "\|\|\\|&&\\|<-R\\|<-\\|==>\\|<=\\|!")
+
+(setq pcvKeywords
+ `((,pcv-kw-regexp . font-lock-keyword-face)
+   (,pcv-builtin-regexp . font-lock-builtin-face)
+   (,pcv-connectives-regexp . font-lock-reference-face)
+   (,pcv-bad-kw-regexp . font-lock-warning-face)
+   (,pcv-bad-builtin-regexp . font-lock-warning-face)
+  )
+)
+
+(define-derived-mode pcv-mode fundamental-mode
+  (setq font-lock-defaults '(pcvKeywords))
+  (setq mode-name "Pro-Crypto-Verif")
+
+;; comment highlighting
+  (modify-syntax-entry ?\( "()1" pcv-mode-syntax-table)
+  (modify-syntax-entry ?\) ")(4" pcv-mode-syntax-table)
+  (modify-syntax-entry ?* ". 23" pcv-mode-syntax-table)
+
+;; _ and ' belong to ordinary identifiers
+  (modify-syntax-entry ?_ "w" pcv-mode-syntax-table)
+  (modify-syntax-entry ?' "w" pcv-mode-syntax-table)
+)
