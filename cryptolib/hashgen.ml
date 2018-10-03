7e7d8d57e716667373e801d49a323dbeec0c945a
Bruno BLANCHET <Bruno.Blanchet@inria.fr>
2018-10-03 17:59:14 +0200

    improved treatment of collisions in PRP, SPRP, and ICM

--- cryptolib/hashgen.ml
+++ cryptolib/hashgen.ml
@@ -249,7 +249,7 @@ let enc_dec_oracle(ck: cipherkey) =
 
 def ICM_cipher(cipherkey, key, blocksize, enc, dec, enc_dec_oracle, qE, qD) {
 
-param Ne, Nd, Nck.
+param Ne, Nd, Necoll, Ndcoll, Nck.
 
 fun enc(cipherkey, blocksize, key): blocksize.
 fun dec(cipherkey, blocksize, key): blocksize.
@@ -266,8 +266,10 @@ equation forall ck:cipherkey, m1:blocksize, m2:blocksize, k:key;
 equiv(icm(enc))
        foreach ick <= Nck do ck <-R cipherkey;
          (foreach ie <= Ne do Oenc(me:blocksize, ke:key) := return(enc(ck, me, ke)) |
-          foreach id <= Nd do Odec(md:blocksize, kd:key) := return(dec(ck, md, kd)))
-     <=((#Oenc+#Odec)*(#Oenc+#Odec-1)*Pcoll2rand(blocksize))=>
+          foreach id <= Nd do Odec(md:blocksize, kd:key) := return(dec(ck, md, kd)) |
+	  foreach iecoll <= Necoll do Oenccoll(me':blocksize, ke':key, re': blocksize) := return(enc(ck, me', ke') = re') |
+	  foreach idcoll <= Ndcoll do Odeccoll(md':blocksize, kd':key, rd': blocksize) := return(dec(ck, md', kd') = rd'))
+     <=((#Oenc+#Odec)*(#Oenc+#Odec-1)*Pcoll2rand(blocksize) + (#Oenccoll + #Odeccoll) * Pcoll1rand(blocksize))=>
        foreach ick <= Nck do 
          (foreach ie <= Ne do Oenc(me:blocksize, ke:key) :=
 		find[unique] j<=Ne suchthat defined(me[j],ke[j],re[j]) && me = me[j] && ke = ke[j] then return(re[j]) 
@@ -276,7 +278,15 @@ equiv(icm(enc))
           foreach id <= Nd do Odec(md:blocksize, kd:key) :=
 		find[unique] j<=Ne suchthat defined(me[j],ke[j],re[j]) && md = re[j] && kd = ke[j] then return(me[j]) 
 		orfind k<=Nd suchthat defined(rd[k],md[k],kd[k]) && md = md[k] && kd = kd[k] then return(rd[k]) 
-		else rd <-R blocksize; return(rd)).
+		else rd <-R blocksize; return(rd) |
+	  foreach iecoll <= Necoll do Oenccoll(me':blocksize, ke':key, re': blocksize) :=
+		find[unique] j<=Ne suchthat defined(me[j],ke[j],re[j]) && me' = me[j] && ke' = ke[j] then return(re' = re[j]) 
+		orfind k<=Nd suchthat defined(rd[k],md[k],kd[k]) && me' = rd[k] && ke' = kd[k] then return(re' = md[k]) 
+		else return(false) |
+          foreach idcoll <= Ndcoll do Odeccoll(md':blocksize, kd':key, rd': blocksize) :=
+		find[unique] j<=Ne suchthat defined(me[j],ke[j],re[j]) && md' = re[j] && kd' = ke[j] then return(rd' = me[j]) 
+		orfind k<=Nd suchthat defined(rd[k],md[k],kd[k]) && md' = md[k] && kd' = kd[k] then return(rd' = rd[k]) 
+		else return(false)).
 
 (* The difference of probability is the probability of collision between two
 random numbers in blocksize among the N+N2 chosen random numbers. *)
